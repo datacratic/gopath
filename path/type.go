0c913512fd337233c8c18c7920d992c62427f5f2
Rémi Attab <remi@datacratic.com>
2015-02-13 16:58:43 -0500

    Added type getter function

---
+++ path/type.go
@@ -0,0 +1,22 @@
+// Copyright (c) 2014 Datacratic. All rights reserved.
+
+package path
+
+import (
+	"reflect"
+)
+
+// Type returns the type of the first value in the given object that matches the
+// path. Returns ErrMissing if the path could not be completed due to a nil
+// field, a missing array index or a missing map value.
+func (path P) Type(obj interface{}) (result reflect.Type, err error) {
+	fn := func (_ P, ctx *Context) (bool, error) {
+		result = ctx.Value().Type()
+		return false, nil
+	}
+
+	err = path.Apply(obj, &Context{Fn: fn})
+	return
+}
+
+
